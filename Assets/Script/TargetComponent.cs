40fb738fce05d796c3195093fe5ca57a927ba0f4
UAlaina <urmicaa@gmail.com>
2025-09-10 17:02:56 -0400

    finished

---
+++ Assets/Script/TargetComponent.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetComponent : MonoBehaviour
+{
+    private Renderer targetRenderer;
+    private Color originalColor;
+    public Color hitColor = Color.red;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRenderer = GetComponent<Renderer>();
+        if (targetRenderer != null)
+        {
+            originalColor = targetRenderer.material.color;
+        }
+    }
+
+    private void OllisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            GameManager.Instance.IncrementScore();
+
+            if (targetRenderer != null)
+            {
+                targetRenderer.material.color = hitColor;
+            }
+
+            Invoke("RestColor", 5f);
+        }
+    }
+
+    private void ResetColor()
+    {
+        if (targetRenderer != null)
+        {
+            targetRenderer.material.color = originalColor;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
